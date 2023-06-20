00ebd3ec99e952cb6bde9c6fe2979a6fa351beed
Crislâine Santos <criis.santos@outlook.com>
2023-06-20 19:36:04 -0300

    feat: add campaigns endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"emailn/internal/contract"
+	"emailn/internal/domain/campaign"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
 )
 
 func main() {
@@ -14,6 +17,23 @@ func main() {
 	router.Use(middleware.RealIP)
 	router.Use(middleware.Logger)
 	router.Use(middleware.Recoverer)
+
+	service := campaign.Service{}
+
+	router.Post("/campaigns", func(w http.ResponseWriter, r *http.Request) {
+		request := contract.NewCampaign{}
+		render.DecodeJSON(r.Body, &request)
+		id, err := service.Create(request)
+
+		if err != nil {
+			render.Status(r, 400)
+			render.JSON(w, r, map[string]string{"error": err.Error()})
+			return
+		}
+
+		render.Status(r, 202)
+		render.JSON(w, r, map[string]string{"id": id})
+	})
 
 	http.ListenAndServe(":3000", router)
 }
