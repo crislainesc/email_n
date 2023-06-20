90060b73866079247d58eba38c81b37796e201f3
Crislâine Santos <criis.santos@outlook.com>
2023-06-20 11:30:50 -0300

    refactor: use testify assert to test

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -1,21 +1,21 @@
 package campaign
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestNewCampaign(t *testing.T) {
+	assert := assert.New(t)
 	name := "Campaign Name"
 	content := "Campaign Content"
 	contacts := []string{"email@example.com", "email@example.com", "email@example.com"}
 
-	c := NewCampaign(name, content, contacts)
+	campaign := NewCampaign(name, content, contacts)
 
-	if c == nil {
-		t.Error("Campaign is nil")
-	} else if c.Name != name {
-		t.Errorf("NewCampaign() Name = %v, want %v", c.Name, name)
-	} else if c.Content != content {
-		t.Errorf("NewCampaign() Content = %v, want %v", c.Content, content)
-	} else if len(c.Contacts) != len(contacts) {
-		t.Errorf("NewCampaign() Contacts = %v, want %v", c.Contacts, contacts)
-	}
+	assert.NotEqual(campaign, nil)
+	assert.Equal(campaign.Name, name)
+	assert.Equal(campaign.Content, content)
+	assert.Equal(len(campaign.Contacts), len(contacts))
 }
