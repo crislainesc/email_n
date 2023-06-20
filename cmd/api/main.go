93bb3cdf94eda71b0b80314a47685d41a1c3242f
Crislâine Santos <criis.santos@outlook.com>
2023-06-20 19:21:05 -0300

    feat: add api router

---
+++ cmd/api/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+
+	http.ListenAndServe(":3000", router)
+}
