6b30c6ae4ee2d699c5b2cd6ce44282aff703db71
Crislâine Santos <criis.santos@outlook.com>
2023-06-21 16:02:51 -0300

    test: get campaign by id endpoint

---
+++ internal/endpoints/campaigns_get_by_id_test.go
@@ -0,0 +1,50 @@
+package endpoints
+
+import (
+	"emailn/internal/contract"
+	"emailn/internal/domain/campaign"
+	internalmock "emailn/internal/test/mock"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func Test_CampaignsGetById_ShouldReturnCampaign(t *testing.T) {
+	assert := assert.New(t)
+	campaignResponse := contract.GetCampaignByIdResponse{
+		ID:      "1",
+		Name:    fake.Lorem().Text(10),
+		Content: fake.Lorem().Text(20),
+		Status:  campaign.Pending.String(),
+	}
+	service := new(internalmock.CampaignServiceMock)
+	service.On("GetById", mock.Anything).Return(&campaignResponse, nil)
+	handler := Handler{CampaignService: service}
+	req, _ := http.NewRequest("GET", "/", nil)
+	res := httptest.NewRecorder()
+
+	response, status, _ := handler.CampaignGetById(res, req)
+
+	assert.Equal(200, status)
+	assert.Equal(campaignResponse.ID, response.(*contract.GetCampaignByIdResponse).ID)
+	assert.Equal(campaignResponse.Name, response.(*contract.GetCampaignByIdResponse).Name)
+}
+
+func Test_CampaignsGetById_WhenSomethingWrong(t *testing.T) {
+	assert := assert.New(t)
+	service := new(internalmock.CampaignServiceMock)
+	errorExpected := errors.New("something wrong")
+	service.On("GetById", mock.Anything).Return(nil, errorExpected)
+	handler := Handler{CampaignService: service}
+	req, _ := http.NewRequest("GET", "/", nil)
+	res := httptest.NewRecorder()
+
+	_, status, err := handler.CampaignGetById(res, req)
+
+	assert.Equal(404, status)
+	assert.Equal(err.Error(), errorExpected.Error())
+}
