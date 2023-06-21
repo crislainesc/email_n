69e3b1b1a07c73c61ee6220a942d3f9fb39af875
Crislâine Santos <criis.santos@outlook.com>
2023-06-21 17:43:29 -0300

    refactor: adjust campaign and contact struct

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -8,22 +8,25 @@ import (
 )
 
 type Contact struct {
-	Email string `validate:"email"`
+	ID         string `gorm:"size:50"`
+	Email      string `validate:"email" gorm:"size:100"`
+	CampaignId string `gorm:"size:50"`
 }
 
 type Campaign struct {
-	ID        string    `validate:"required"`
-	Name      string    `validate:"min=5,max=24"`
-	Content   string    `validate:"min=5,max=1024"`
+	ID        string    `validate:"required" gorm:"size:50"`
+	Name      string    `validate:"min=5,max=24" gorm:"size:24"`
+	Content   string    `validate:"min=5,max=1024" gorm:"size:1024"`
 	Contacts  []Contact `validate:"min=1,dive"`
 	CreatedOn time.Time `validate:"required"`
-	Status    Status
+	Status    Status    `gorm:"size:20"`
 }
 
 func NewCampaign(name string, content string, emails []string) (*Campaign, error) {
 
 	contacts := make([]Contact, len(emails))
 	for index, email := range emails {
+		contacts[index].ID = xid.New().String()
 		contacts[index].Email = email
 	}
 
