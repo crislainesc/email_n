1d9c63924a22b2c8e6a205bb4bc2dd20280c37be
Crislâine Santos <criis.santos@outlook.com>
2023-06-22 12:31:28 -0300

    feat: add dir and loadEnv functions

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,7 +4,10 @@ import (
 	"emailn/internal/domain/campaign"
 	"emailn/internal/endpoints"
 	"emailn/internal/infra/database"
+	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -12,12 +15,44 @@ import (
 	_ "github.com/joho/godotenv/autoload"
 )
 
-func main() {
-	err := godotenv.Load()
+// loadEnv loads the environment variables from the .env file.
+func loadEnv(envFile string) {
+	err := godotenv.Load(dir(envFile))
 	if err != nil {
-		panic("fail to read environment variables")
+		panic(fmt.Errorf("error loading .env file: %w", err))
+	}
+}
+
+/*
+dir returns the absolute path of the given environment file (envFile) in the Go module's
+root directory. It searches for the 'go.mod' file from the current working directory upwards
+and appends the envFile to the directory containing 'go.mod'.
+It panics if it fails to find the 'go.mod' file.
+*/
+func dir(envFile string) string {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
 	}
 
+	for {
+		goModPath := filepath.Join(currentDir, "go.mod")
+		if _, err := os.Stat(goModPath); err == nil {
+			break
+		}
+
+		parent := filepath.Dir(currentDir)
+		if parent == currentDir {
+			panic(fmt.Errorf("go.mod not found"))
+		}
+		currentDir = parent
+	}
+
+	return filepath.Join(currentDir, envFile)
+}
+
+func main() {
+	loadEnv(".env")
 	router := chi.NewRouter()
 
 	router.Use(middleware.RequestID)
